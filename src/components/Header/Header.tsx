caf68ae7e2aa6ee4fde81e5bfb5d0a7f0da59538
Artem <rozen19942810@gmail.com>
2021-01-06 00:55:40 +0200

    upd header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,19 @@
 import React from "react"
 import s from "./Header.module.scss"
+import logo from "../../assets/icon/parrot.svg"
 
 export const Header = () => {
     return (
         <header className={s.headerBlock}>
-            <b>H E A D E R</b>
+            <div className={s.logo}>
+                <img src={logo} alt=' '/>
+            </div>
+            <div className={s.title}>
+                <span>bingo</span>
+            </div>
+            <div className={s.cart}>
+
+            </div>
         </header>
     )
 }
