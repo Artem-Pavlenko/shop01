497c283fcd3090f82e61f19f7e6c8ab5189e5b4f
Artem <rozen19942810@gmail.com>
2021-01-06 23:16:17 +0200

    upd cart reducer

--- src/store/cartReducer.ts
+++ src/store/cartReducer.ts
@@ -4,7 +4,7 @@ export type CartRootType = {
     cart: Array<Item>
     totalPrice: number
 }
-type ActionTypes = ReturnType<typeof addProductToCart>
+type ActionTypes = ReturnType<typeof addProductToCart> | ReturnType<typeof deleteProductFromCart>
 
 const initState: CartRootType = {
     cart: [],
@@ -12,13 +12,25 @@ const initState: CartRootType = {
 }
 
 
-export const cartReducer = (state:CartRootType = initState, action: ActionTypes): CartRootType => {
+export const cartReducer = (state: CartRootType = initState, action: ActionTypes): CartRootType => {
     switch (action.type) {
         case "cart/ADD_PRODUCT_TO_CART":
-            return {...state, cart: [...state.cart, action.product], totalPrice: state.totalPrice + action.product.prise}
+            return {
+                ...state,
+                cart: [...state.cart, action.product],
+                totalPrice: state.totalPrice + action.product.prise
+            }
+        case "cart/DELETE_PRODUCT":
+            const {prise} = state.cart.find(p => p.id === action.id) as Item
+            return {
+                ...state,
+                cart: state.cart.filter(p => p.id !== action.id),
+                totalPrice: state.totalPrice - (prise)
+            }
         default:
             return state
     }
 }
 
 export const addProductToCart = (product: Item) => ({type: 'cart/ADD_PRODUCT_TO_CART', product} as const)
+export const deleteProductFromCart = (id: string) => ({type: 'cart/DELETE_PRODUCT', id} as const)
