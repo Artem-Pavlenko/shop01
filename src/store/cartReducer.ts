7a057791de2a061ac8e9ffb33aebcffc73be2042
Artem <rozen19942810@gmail.com>
2021-01-06 20:28:09 +0200

    refactor cart(price)

--- src/store/cartReducer.ts
+++ src/store/cartReducer.ts
@@ -2,18 +2,20 @@ import {Item} from "./productsReducer";
 
 export type CartRootType = {
     cart: Array<Item>
+    totalPrice: number
 }
 type ActionTypes = ReturnType<typeof addProductToCart>
 
 const initState: CartRootType = {
-    cart: []
+    cart: [],
+    totalPrice: 0
 }
 
 
 export const cartReducer = (state:CartRootType = initState, action: ActionTypes): CartRootType => {
     switch (action.type) {
         case "cart/ADD_PRODUCT_TO_CART":
-            return {...state, cart: [...state.cart, action.product]}
+            return {...state, cart: [...state.cart, action.product], totalPrice: state.totalPrice + action.product.prise}
         default:
             return state
     }
